4d45550f32dfe8be3b9dc50091f2e72d1aa1b645
amal-007-amal <amalsankarps0@gmail.com>
2025-06-20 13:16:52 +0530

    un unded vaiable updated

--- src/shared/Header.tsx
+++ src/shared/Header.tsx
@@ -1,5 +1,5 @@
 import { AudioLines, LogOut } from "lucide-react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Button } from "../components/ui/button";
 
 export default function Header() {
