6b3d3c163b501b305dd0fab366033ce65899ffe4
amallyonit <amalsankar.ps@lyonit.com>
2025-07-10 18:31:50 +0530

    updated the bg hover

--- src/components/aethermultiselect.tsx
+++ src/components/aethermultiselect.tsx
@@ -79,7 +79,7 @@ export function AetherMultiSelect({
           <CommandInput placeholder="Search..." />
           <CommandEmpty>No results found.</CommandEmpty>
           <div className="flex justify-between items-center px-3 py-2 text-xs border-b">
-            <button onClick={handleSelectAll} className="text-black hover:underline">
+            <button onClick={handleSelectAll} className="hover:underline">
               Select All
             </button>
             <div className="flex gap-2">
