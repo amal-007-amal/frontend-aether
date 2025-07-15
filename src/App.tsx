01632f6af90f3bdcdbea8810affde024791aaa10
amal-007-amal <amalsankarps0@gmail.com>
2025-07-15 21:44:26 +0530

    updated the aether_table_col

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { useEffect } from 'react';
 import './App.css';
 import { TooltipProvider } from './components/ui/tooltip';
 import AetherAppRouter from './routes';
 import { Toaster } from 'sonner';
 
 function App() {
+
+    useEffect(() => {
+    const stored = localStorage.getItem("aether_table_col");
+
+    if (stored) {
+      try {
+        const parsed = JSON.parse(stored);
+
+        const allHaveMapkey = Array.isArray(parsed) && parsed.every((item) => 'mapkey' in item);
+
+        if (!allHaveMapkey) {
+          localStorage.removeItem("aether_table_col");
+        }
+      } catch (err) {
+        localStorage.removeItem("aether_table_col");
+      }
+    }
+  }, []);
 
   return (
     <TooltipProvider>
